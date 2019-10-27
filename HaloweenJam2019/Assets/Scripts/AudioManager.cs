eb8ece10988df5a72a1cc5b42cdee60e8cb13cab
Mint16384 <mint16384@gmail.com>
2019-10-27 19:19:21 -0400

    zombies make sound

--- HaloweenJam2019/Assets/Scripts/AudioManager.cs
+++ HaloweenJam2019/Assets/Scripts/AudioManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
 
-
+    void Start()
+    {
+        StartCoroutine(DoClips());
+    }
+    IEnumerator DoClips()
+    {
+        yield return new WaitForSeconds(Random.Range(10, 25));
+        audioSource.PlayOneShot(audioClips[(int)Random.Range(0, audioClips.Length - 1)]);
+        StartCoroutine(DoClips());
+    }
 }
