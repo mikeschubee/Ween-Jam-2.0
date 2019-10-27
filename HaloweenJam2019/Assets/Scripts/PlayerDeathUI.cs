a9868f9643ae0b5f6124cf9ba3d9eaf6925459e2
JackHill-SJ <hilljack1856@gmail.com>
2019-10-27 16:22:59 -0400

    Buttons DeathUI

---
+++ HaloweenJam2019/Assets/Scripts/PlayerDeathUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeathUI : MonoBehaviour
+{
+    public Canvas canvas;
+    public GameObject eventSystem;
+
+    void Start()
+    {
+        canvas.enabled = false;
+        eventSystem = GameObject.FindGameObjectWithTag("EventSystem");
+    }
+
+    private void Update()
+    {
+        PlayerScore isScoring = eventSystem.GetComponent<PlayerScore>();
+        if (isScoring == false)
+        {
+            canvas.enabled = true;
+        }
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ HaloweenJam2019/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+    public Text scoreText;
+    public float score;
+    public float pointIncrease;
+    public bool isScoring;
+
+    private void Start()
+    {
+        isScoring = true;
+    }
+
+    private void Update()
+    {
+        if (isScoring == true)
+        {
+            score += pointIncrease * Time.deltaTime;
+        }
+
+        if (isScoring == false)
+        {
+            scoreText.text = "Time Survived:" + (int)score;
+        }
+    }
+}
