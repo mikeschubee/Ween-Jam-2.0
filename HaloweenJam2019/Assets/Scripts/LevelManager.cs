555c7805b69cae8d776ac0dac6baaeb1dff63937
JackHill-SJ <hilljack1856@gmail.com>
2019-11-29 20:16:47 -0800

    Revert "Updated Levels"
    This reverts commit 7b4194b8f1efb14385542873901a2737eb2b0f30, reversing
    changes made to 58a5d544740cd448132d847b3f1f7d779d6cb799.

--- HaloweenJam2019/Assets/Scripts/LevelManager.cs
+++ HaloweenJam2019/Assets/Scripts/LevelManager.cs
@@ -11,23 +11,10 @@ public class LevelManager : MonoBehaviour
     {
         StartCoroutine(LoadSceneAsync(sceneIndex));
     }
-    public void UnloadScene(int sceneIndex)
-    {
-        StartCoroutine(UnloadSceneAsync(sceneIndex));
-    }
 
     IEnumerator LoadSceneAsync(int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-        while (!operation.isDone)
-        {
-            yield return null;
-        }
-    }
-
-    IEnumerator UnloadSceneAsync(int sceneIndex)
-    {
-        AsyncOperation operation = SceneManager.UnloadSceneAsync(sceneIndex);
         while (!operation.isDone)
         {
             yield return null;
